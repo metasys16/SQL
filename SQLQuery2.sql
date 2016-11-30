cd200a7801f55ee6197bcb33de66a5296aace43f
metasys16 <pereira.mickael1@gmail.com>
2016-11-30 15:54:02 +0100

    script SQL CREATE terminé

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -8,7 +8,7 @@ CREATE TABLE "Etudiant" (
 	CONSTRAINT pk_etudiant
 		PRIMARY KEY(id),
 
-	CONSTRAINT fk_etudiant_promotion
+	CONSTRAINT fk_etudiantPromotion
 		FOREIGN KEY(id_promotion) REFERENCES Promotion(id)
 );
 
@@ -22,7 +22,7 @@ CREATE TABLE "Promotion" (
 	CONSTRAINT pk_promotion
 		PRIMARY KEY(id),
 
-	CONSTRAINT fk_promotion_formation
+	CONSTRAINT fk_promotionFormation
 		FOREIGN KEY(id_formation) REFERENCES Formation(id)
 );
 
@@ -53,11 +53,11 @@ CREATE TABLE "Matiere" (
 		PRIMARY KEY(id)
 );
 
-CREATE TABLE "Formation_Matiere" (
+CREATE TABLE "Formation_matiere" (
 	id_formation INTEGER NOT NULL,
 	id_matiere INTEGER NOT NULL,
 
-	CONSTRAINT pk_formation_matiere
+	CONSTRAINT pk_formationMatiere
 		PRIMARY KEY(id_formation, id_matiere),
 
 	CONSTRAINT fk_formation
@@ -78,19 +78,40 @@ CREATE TABLE "Salle" (
 CREATE TABLE "Session" (
 	id INTEGER NOT NULL,
 	date_debut DATETIME,
-	date_fin DATETIME
+	date_fin DATETIME,
+	nom_salle VARCHAR(50) NOT NULL,
+	nom_formateur VARCHAR(50) NOT NULL,
+	id_promotion INTEGER NOT NULL,
+	id_matiere INTEGER NOT NULL
 
 	CONSTRAINT pk_session	
 		PRIMARY KEY(id)
+
+	CONSTRAINT fk_salle
+		FOREIGN KEY(nom_salle) REFERENCES Salle(nom),
+
+	CONSTRAINT fk_formateur
+		FOREIGN KEY(nom_formateur) REFERENCES Formateur(nom),
+
+	CONSTRAINT fk_promotion
+		FOREIGN KEY(id_promotion) REFERENCES Promotion(id),
+
+	CONSTRAINT fk_matiere
+		FOREIGN KEY(id_matiere) REFERENCES Matiere(id),
+
 );
 
 CREATE TABLE "EmploiDuTemps" (
 	id INTEGER NOT NULL,
 	date_debut DATETIME,
-	date_fin DATETIME
+	date_fin DATETIME,
+	id_session INTEGER NOT NULL
 
-	CONSTRAINT pk_EmploiDuTemps
+	CONSTRAINT pk_emploiDuTemps
 		PRIMARY KEY(id)
+
+	CONSTRAINT fk_emploiDuTemps
+		FOREIGN KEY(id_session) REFERENCES "Session"(id),
 );
 
 --CREATE TABLE "Absence" ();
