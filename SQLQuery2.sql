d4b37ce4e9ad6fca1293f5c8d7bbba5bf6f308c7
metasys16 <pereira.mickael1@gmail.com>
2016-11-30 16:28:53 +0100

    script SQL CREATE terminé v3

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,9 +1,34 @@
+DROP TABLE Formation;
+
+CREATE TABLE "Formation" (
+	id INTEGER NOT NULL,
+	nom VARCHAR(50) NOT NULL,
+	nb_heures_total FLOAT NOT NULL
+
+	CONSTRAINT pk_formation
+		PRIMARY KEY(id),
+);
+
+CREATE TABLE "Promotion" (
+	id INTEGER NOT NULL,
+	nom VARCHAR(50) NOT NULL,
+	date_debut DATETIME,
+	date_fin DATETIME,
+	id_formation INTEGER UNIQUE NOT NULL,	
+
+	CONSTRAINT pk_promotion
+		PRIMARY KEY(id),
+
+	CONSTRAINT fk_promotionFormation
+		FOREIGN KEY(id_formation) REFERENCES Formation(id)
+);
+
 CREATE TABLE "Etudiant" (
 	id INTEGER UNIQUE NOT NULL,
 	nom VARCHAR(50) NOT NULL,
 	prenom VARCHAR(50) NOT NULL,
 	mail VARCHAR(50) UNIQUE NOT NULL,
-	id_promotion INTEGER NOT NULL,	
+	id_promotion INTEGER UNIQUE NOT NULL,	
 	
 	CONSTRAINT pk_etudiant
 		PRIMARY KEY(id),
@@ -12,19 +37,6 @@ CREATE TABLE "Etudiant" (
 		FOREIGN KEY(id_promotion) REFERENCES Promotion(id)
 );
 
-CREATE TABLE "Promotion" (
-	id INTEGER NOT NULL,
-	nom VARCHAR(50) NOT NULL,
-	date_debut DATETIME,
-	date_fin DATETIME,
-	id_formation INTEGER NOT NULL,	
-
-	CONSTRAINT pk_promotion
-		PRIMARY KEY(id),
-
-	CONSTRAINT fk_promotionFormation
-		FOREIGN KEY(id_formation) REFERENCES Formation(id)
-);
 
 CREATE TABLE "Formateur" (
 	id INTEGER NOT NULL,
@@ -37,21 +49,20 @@ CREATE TABLE "Formateur" (
 		PRIMARY KEY(id)
 );
 
-CREATE TABLE "Formation" (
-	id INTEGER NOT NULL,
-	nom VARCHAR(50) NOT NULL,
-	nb_heures_total FLOAT NOT NULL
-
-	CONSTRAINT pk_formation
-		PRIMARY KEY(id)
-);
-
 CREATE TABLE "Matiere" (
 	id INTEGER NOT NULL,
 	nom VARCHAR(50) NOT NULL
 
 	CONSTRAINT pk_matiere
 		PRIMARY KEY(id)
+);
+
+CREATE TABLE "Salle" (
+	id INTEGER NOT NULL,
+	nom VARCHAR(50) NOT NULL,
+	capacite INT 
+
+	PRIMARY KEY(id)
 );
 
 CREATE TABLE "Formation_matiere" (
@@ -66,15 +77,6 @@ CREATE TABLE "Formation_matiere" (
 
 	CONSTRAINT fk_matiere
 		FOREIGN KEY(id_matiere) REFERENCES Matiere(id)
-);
-
-
-CREATE TABLE "Salle" (
-	id INTEGER NOT NULL,
-	nom VARCHAR(50) NOT NULL,
-	capacite INT 
-
-	PRIMARY KEY(id)
 );
 
 CREATE TABLE "Session" (
@@ -103,17 +105,20 @@ CREATE TABLE "Session" (
 
 );
 
-CREATE TABLE "EmploiDuTemps" (
-	id INTEGER NOT NULL,
-	date_debut DATETIME,
-	date_fin DATETIME,
-	id_session INTEGER NOT NULL
 
-	CONSTRAINT pk_emploiDuTemps
-		PRIMARY KEY(id)
+CREATE TABLE "Absence" (
 
-	CONSTRAINT fk_emploiDuTemps
-		FOREIGN KEY(id_session) REFERENCES "Session"(id),
+	id_etudiant INTEGER NOT NULL,
+	id_session INTEGER NOT NULL,
+
+	CONSTRAINT pk_Absence
+		PRIMARY KEY(id_etudiant, id_session),
+
+	CONSTRAINT fk_etudiant
+		FOREIGN KEY(id_etudiant) REFERENCES Etudiant(id),
+
+	CONSTRAINT fk_session
+		FOREIGN KEY(id_session) REFERENCES "Session"(id)
 );
 
---CREATE TABLE "Absence" ();
+--CREATE TABLE "EmploiDuTemps" (); Pas besoin d'enregistrer notre emploi du temps dans la base, c'est un Objet qu'on va se servir tout le temps. On fera peut etre un .pdf à enregistrer sur serveur
