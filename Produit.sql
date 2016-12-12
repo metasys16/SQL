06b073eea81d2058d287e08918a7d20f935f5eb4
metasys16 <pereira.mickael1@gmail.com>
2016-12-12 11:19:54 +0100

    Script exo produit

--- Produit.sql
+++ Produit.sql
@@ -1,19 +1,20 @@
+CREATE TABLE categorie (
+	id INTEGER NOT NULL,
+	libelle VARCHAR(50) NOT NULL,
+	
+	CONSTRAINT pk_categorie
+		PRIMARY KEY(id)
+);
+
 CREATE TABLE produit (
 	id INTEGER NOT NULL,
 	nom VARCHAR(50) NOT NULL,
 	code CHAR(5) UNIQUE NOT NULL,
-
-	CONSTRAINT pk_produit
-		PRIMARY KEY(id)
-);
-
-CREATE TABLE categorie (
-	id INTEGER NOT NULL,
-	libelle VARCHAR(50) NOT NULL,
-	id_produit INTEGER UNIQUE NOT NULL,
+	id_categorie INTEGER UNIQUE NOT NULL,
 
 	CONSTRAINT pk_produit
 		PRIMARY KEY(id),
 	CONSTRAINT fk_produitCategorie
-		FOREIGN KEY (id_produit) REFERENCES produit(id)
+		FOREIGN KEY (id_categorie) REFERENCES categorie(id)
 );
+
